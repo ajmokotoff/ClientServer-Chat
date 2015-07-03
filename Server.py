30b82670de5ef92a2a31bb9dc3796eb38d4f0df3
Mok <amokotoff@gmail.com>
2015-07-02 20:02:23 -0400

    Added GUI Support

--- Server.py
+++ Server.py
@@ -21,7 +21,6 @@ def chat_server():
     # add server socket object to the list of readable connections
     SOCKET_LIST.append(server_socket)
 
- 
     print "Chat server started on port " + str(PORT)
  
     while 1:
@@ -29,6 +28,7 @@ def chat_server():
         # get the list sockets which are ready to be read through select
         # 4th arg, time_out  = 0 : poll and never block
         ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+        memberList=""
       
         for sock in ready_to_read:
             # a new connection request recieved
@@ -37,6 +37,11 @@ def chat_server():
                 SOCKET_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
                 broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+                for name in MEMBERS:
+                    if name.lower() != message[0].lower():
+                        memberList += name + "\n"
+                    MEMBERS[message[0]].send("query"+memberList)
+                    memberList = ""
              
             # a message from a client, not a new connection
             else:
@@ -49,6 +54,7 @@ def chat_server():
                         # broadcast(server_socket, sock,
                         #  "\r" + '[' + str(sock.getpeername()) + '] ' + data)
                         message = data.split(": ")
+                        print message
                         nick = message[0]
                         msg = message[1]
 
@@ -62,30 +68,45 @@ def chat_server():
 
                         if m:
                             print "Private message"
-                            target = m.group(0)
-                            print target
-                            try:
+                            target = m.group(1)
+                            print m.group(2)
 
+                            try:
                                 if target in MEMBERS:
-                                    MEMBERS[target].send(data)
+                                    MEMBERS[target].send("Private message from "+message[0] + ": " + m.group(2) + "\n")
                                 else:
                                     print "No target"
                             except Exception as e:
                                 print e.message
                                 MEMBERS[target].close()
                             pass
+
+                        elif msg[:len(msg)-1] == "queryusers":
+                            print('it worked...')
+                            for name in MEMBERS:
+                                if name.lower() != message[0].lower():
+                                    memberList += name + "\n"
+                            MEMBERS[message[0]].send("query"+memberList)
+                            memberList = ""
+
                         else:
                             broadcast(server_socket, sock, data)
                     else:
-                        # remove the socket that's broken    
+                        # remove the socket that's broken
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
-                        print 'Docket that\'s broken'
+                        print 'Socket that\'s broken'
                         # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                        for name in MEMBERS:
+                            if name.lower() != message[0].lower():
+                                memberList += name + "\n"
+                            MEMBERS[message[0]].send("query"+memberList)
+                            memberList = ""
 
-                # exception 
+                # exception
                 except Exception as e:
+                    print "here?"
                     print e
                     broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
                     continue
