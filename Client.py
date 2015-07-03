9d8946b506516863aa6a4a57ebc56ff1580ffbc0
Mok <amokotoff@gmail.com>
2015-07-02 20:00:56 -0400

    Added GUI Support

--- Client.py
+++ Client.py
@@ -1,32 +1,99 @@
-from Tkinter import *
 import sys
 import socket
 import select
 
 
+class ChatClient:
+    s = None
+    socket = None
+    host = '127.0.0.1'
+    port = 9009
+    nickname = 'Mok'
+    members = []
+
+    def __init__(self, host, nickname):
+        ChatClient.host = host
+        ChatClient.nickname = nickname
+
+    @staticmethod
+    def connect():
+        ChatClient.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ChatClient.socket.settimeout(2)
+        try:
+            ChatClient.socket.connect((ChatClient.host, ChatClient.port))
+        except:
+            print 'Unable to connect'
+            sys.exit()
+        ChatClient.socket.send(ChatClient.nickname + ": Has joined the channel\n")
+        sys.stdout.write('[Me] ');
+        sys.stdout.flush()
+
+    @staticmethod
+    def listener():
+        socket_list = [sys.stdin, ChatClient.socket]
+
+        # Get the list sockets which are readable
+        ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
+
+        for sock in ready_to_read:
+            if sock == ChatClient.socket:
+                # incoming message from remote server, s
+                data = sock.recv(4096)
+                if not data:
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else:
+                    # print data
+                    sys.stdout.write('\r' + data)
+                    sys.stdout.write('[Me] ');
+                    sys.stdout.flush()
+                    return data
+            '''
+            else:
+                # user entered a message
+                msg = sys.stdin.readline()
+
+                if msg[0] == 'q' or msg[0] == 'Q':
+                    sys.exit()
+
+                else:
+                    ChatClient.socket.send(ChatClient.nickname + ": " + msg)
+                    sys.stdout.write('[Me] ');
+                    sys.stdout.flush()
+            '''
+
+    @staticmethod
+    def send_message(msg):
+        #msg = sys.stdin.readline()
+
+        if msg[0].lower() == 'q' and len(msg) < 3:
+            sys.exit()
+
+        else:
+            ChatClient.socket.send(ChatClient.nickname + ": " + msg)
+            sys.stdout.write('[Me] ');
+            sys.stdout.flush()
+
+    @staticmethod
+    def exit():
+        sys.exit()
+
+
+
 def chat_client():
-
-
     '''
-    window = Tk()
-    label = Label(window, text='our label widget')
-    entry = Entry(window)
-    label.pack(side=TOP)
-    entry.pack()
-    window.mainloop()
-    '''
-
     if len(sys.argv) < 4:
         print 'Usage : python chat_client.py hostname port nickname'
         sys.exit()
+    '''
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-    nickname = sys.argv[3]
+    host = '127.0.0.1'
+    port = int(9009)
+    nickname = "joddsh"
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
-     
+
     # connect to remote host
     try:
         s.connect((host, port))
@@ -38,14 +105,14 @@ def chat_client():
     print 'Connected to remote host. You can start sending messages'
     sys.stdout.write('[Me] ');
     sys.stdout.flush()
-     
+
     while 1:
         socket_list = [sys.stdin, s]
-         
+
         # Get the list sockets which are readable
         ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [])
-         
-        for sock in ready_to_read:             
+
+        for sock in ready_to_read:
             if sock == s:
                 # incoming message from remote server, s
                 data = sock.recv(4096)
@@ -53,23 +120,26 @@ def chat_client():
                     print '\nDisconnected from chat server'
                     sys.exit()
                 else:
-                    # print data
-                    sys.stdout.write('\r' + data)
-                    sys.stdout.write('[Me] ');
-                    sys.stdout.flush()
-            
+                    if data[:5] == 'query':
+                        data = data[5:len(data)-1]
+                        print data
+                    else:
+                        sys.stdout.write('\r' + data)
+                        sys.stdout.write('[Me] ');
+                        sys.stdout.flush()
+
             else:
                 # user entered a message
                 msg = sys.stdin.readline()
 
-
-                if msg[0] == 'q' or msg[0] == 'Q':
+                if msg[0].lower() == 'q' and len(msg) < 3:
                     sys.exit()
 
                 else:
                     s.send(nickname + ": " + msg)
                     sys.stdout.write('[Me] ');
                     sys.stdout.flush()
+
 
 if __name__ == "__main__":
     sys.exit(chat_client())
---
+++ GUI.py
@@ -0,0 +1,149 @@
+from Tkinter import *
+import time
+import threading
+import random
+import Queue
+from Client import ChatClient
+
+
+def send(message, instance):
+    msg = "[Me]: " + message + "\n"
+    ChatClient.send_message(message + "\n")
+    instance.text_input.delete(0, END)
+    instance.queue.put(msg)
+
+def exit_chat():
+    client.endApplication()
+    ChatClient.exit()
+    sys.exit()
+
+
+class GuiPart:
+    def __init__(self, master, queue, endCommand):
+        self.queue = queue
+        self.bottom_frame = Frame(master)
+        self.bottom_frame.pack(side=BOTTOM)
+
+        self.member_frame = Frame(master)
+        self.member_frame.pack(side=LEFT)
+
+        self.member_list = Text(self.member_frame)
+        self.member_list.configure(state="disabled")
+        self.member_list.pack()
+
+        # Set up the GUI
+        self.quit_button = Button(self.bottom_frame, text='Quit', command=endCommand)
+        self.quit_button.pack(side=LEFT)
+
+        self.scrollbar = Scrollbar(master)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.log = Text(master, width=50, height=30, takefocus=0)
+        self.log.configure(state="disabled")
+        self.log.pack()
+
+        # attach text box to scrollbar
+        self.log.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.log.yview)
+
+        self.text_input = Entry(master, width=45)
+        self.text_input.bind("<Return>",(lambda event: send(self.text_input.get(), self)))
+        self.text_input.pack(side=BOTTOM)
+
+        self.send_button = Button(self.bottom_frame, text="Send Message")
+        self.send_button.pack(side=RIGHT)
+
+    def processIncoming(self):
+        """
+        Handle all the messages currently in the queue (if any).
+        """
+        while self.queue.qsize():
+            try:
+                msg = self.queue.get(0)
+                # Check contents of message and do what it says
+                # As a test, we simply print it
+                print msg
+                if msg[:5] == 'query':
+                    msg = "Online Users:\n" + msg[5:len(msg)-1]
+                    self.member_list.configure(state="normal")
+                    self.member_list.delete("1.0", END)
+                    self.member_list.insert(END, msg)
+                    self.member_list.configure(state="disable")
+                else:
+                    self.log.configure(state="normal")
+                    self.log.insert(END, str(msg))
+                    self.log.configure(state="disable")
+                    # to prevent user from typing in window
+
+            except Queue.Empty:
+                pass
+
+
+class ThreadedClient:
+    """
+    Launch the main part of the GUI and the worker thread. periodicCall and
+    endApplication could reside in the GUI part, but putting them here
+    means that you have all the thread controls in a single place.
+    """
+    def __init__(self, master):
+        """
+        Start the GUI and the asynchronous threads. We are in the main
+        (original) thread of the application, which will later be used by
+        the GUI. We spawn a new thread for the worker.
+        """
+        self.master = master
+
+        # Create the queue
+        self.queue = Queue.Queue()
+
+        # Set up the GUI part
+        self.gui = GuiPart(master, self.queue, self.endApplication)
+
+        # Set up the thread to do asynchronous I/O
+        # More can be made if necessary
+        self.running = 1
+        self.thread1 = threading.Thread(target=self.workerThread1)
+        self.thread1.start()
+
+        # Start the periodic call in the GUI to check if the queue contains
+        # anything
+        self.periodicCall()
+
+    def periodicCall(self):
+        """
+        Check every 100 ms if there is something new in the queue.
+        """
+        self.gui.processIncoming()
+        if not self.running:
+            # This is the brutal stop of the system. You may want to do
+            # some cleanup before actually shutting it down.
+            import sys
+            sys.exit(1)
+        self.master.after(100, self.periodicCall)
+
+    def workerThread1(self):
+        """
+        This is where we handle the asynchronous I/O. For example, it may be
+        a 'select()'.
+        One important thing to remember is that the thread has to yield
+        control.
+        """
+        ChatClient.connect()
+
+        while self.running:
+
+            message = ChatClient.listener()
+            if message is not None and len(message) > 1:
+                self.queue.put(message)
+
+    def endApplication(self):
+        self.running = 0
+        self.thread1.join()
+        sys.exit()
+
+rand = random.Random()
+root = Tk()
+root.wm_title("Chat Room")
+
+client = ThreadedClient(root)
+root.mainloop()
